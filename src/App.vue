d0ca57801bcf3e9b008dc345368b9fd0b93cb0bb
Jinliang <miku.cy@foxmail.com>
2024-06-13 11:12:33 +0800

    chore: 引入代码规范

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-
 <template>
   <div class="app">
-    <blog_header/>
-    <top_img/>
-    <login_box/>
-    <test01/>
+    <blog_header />
+    <top_img />
+    <login_box />
+    <test01 />
   </div>
 </template>
 
 <script lang="ts">
-  import blog_header from './components/blog_header.vue'
-  import top_img from './components/top_img.vue'
-  import login_box from './components/login_box.vue'
-  import test01 from './components/test01.vue'
+import blog_header from './components/blog_header.vue'
+import top_img from './components/top_img.vue'
+import login_box from './components/login_box.vue'
+import test01 from './components/test01.vue'
 
-  export default{
-    name:'App',
-    components:{blog_header,top_img,login_box,test01}
-  }
+export default {
+  name: 'App',
+  components: { blog_header, top_img, login_box, test01 },
+}
 </script>
 
 <style>
-  html, body #app{
-    margin: 0;
-    padding: 0;
-    height: 100%;
-    width: 100%;
-  }
+html,
+body #app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
 </style>
